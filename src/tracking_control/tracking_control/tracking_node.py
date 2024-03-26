301880bdf85c67e147c91d2f74e86a18ea5e46c7
acardone2000 <90355373+acardone2000@users.noreply.github.com>
2024-03-25 23:49:09 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -240,7 +240,7 @@ class TrackingNode(Node):
 
         #Dynamic gain adjustment factor
         linear_gain_factor = 0.7
-        angular_gain_factor = 1.2
+        angular_gain_factor = 0.9
 
        
 
