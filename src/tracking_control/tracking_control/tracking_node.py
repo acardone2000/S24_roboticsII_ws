867d01a61ed53bc81e65e57935ad5fe5253af09f
quirox154 <152574335+quirox154@users.noreply.github.com>
2024-03-26 12:56:32 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -239,8 +239,8 @@ class TrackingNode(Node):
         # TODO: Update the control velocity command
 
         #Dynamic gain adjustment factor
-        linear_gain_factor = 0.7
-        angular_gain_factor = 0.7
+        linear_gain_factor = 0.9
+        angular_gain_factor = 0.9
 
        
 
