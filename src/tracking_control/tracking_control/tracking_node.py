d7dddecb1bdca98474035f570c8b774e0f0722c2
acardone2000 <90355373+acardone2000@users.noreply.github.com>
2024-03-27 23:19:52 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -248,7 +248,7 @@ class TrackingNode(Node):
         distance = np.linalg.norm(self.obj_pose[:2]) #distance based on x and y mesurments
         angle = math.atan2(self.obj_pose[1], self.obj_pose[0]) #Angle to object
 
-        linear_gain = self.linear_gain_base -linear_gain_factor * (distance - self.stop_distance)
+        linear_gain = self.linear_gain_base + linear_gain_factor * (distance - self.stop_distance)
         angular_gain = self.angular_gain_base + angular_gain_factor * abs(angle)
 
         linear_gain=max(linear_gain, 0.1)
